eb09d8f30277d76e175decab3d5028fed5afb85a
Tian(Maxwell) Yang <maxwell_yang@outlook.com>
2023-06-29 16:01:37 +0800

    Add zip file downloading

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import os
 import pika
-from fastapi import FastAPI, UploadFile, Depends
+import aiofiles
+from fastapi import Form, File, FastAPI, UploadFile, Depends
 from sqlalchemy.orm import Session
 
 from database import engine
@@ -7,6 +9,8 @@ import models
 import schemas
 import utils
 
+
+SCRATCH_DIR = "scratch"
 
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
@@ -46,8 +50,48 @@ async def check_git_repos(
     return new_task
 
 @app.post("/check_zip_file")
-async def check_zip_file(zip_file: UploadFile):
-    return {"message": "Hello World"}
+async def check_zip_file(
+    file: str = Form(...),
+    language: str = Form(...),
+    function_name: str = Form(default="*"),
+    threshold: int = Form(default=5),
+    zip_file: UploadFile = File(...),
+    db: Session = Depends(utils.get_db),
+):
+    zip_filename = utils.gen_zip_file_dirname()+".zip"
+    async with aiofiles.open(os.path.join(SCRATCH_DIR, zip_filename), "wb") as out_file:
+        while content := await zip_file.read(1024):
+            await out_file.write(content)
+    
+    new_task = models.Task()
+    db.add(new_task)
+    db.commit()
+    db.refresh(new_task)
+
+    request_data = {
+        "zip_filename": zip_filename,
+        "file": file,
+        "language": language,
+        "function_name": function_name,
+        "threshold": threshold,
+        "task_id": new_task.id,
+    }
+
+    # connection = pika.BlockingConnection(
+    #     pika.ConnectionParameters(host='localhost'))
+    # channel = connection.channel()
+
+    # channel.queue_declare(queue='mayat')
+
+    # channel.basic_publish(
+    #     exchange='',
+    #     routing_key='mayat_zip',
+    #     body=request_data
+    # )
+
+    # connection.close()
+
+    return new_task
 
 @app.get("/task/{task_id}")
 async def get_result(task_id: int, db: Session = Depends(utils.get_db)):
--- utils.py
+++ utils.py
@@ -6,6 +6,9 @@ from database import SessionLocal
 def gen_repos_root_dirname():
     return f"github_repos_{uuid.uuid4()}"
 
+def gen_zip_file_dirname():
+    return f"zip_file_{uuid.uuid4()}"
+
 def gen_repo_dirname(repo_url):
     return repo_url.split('/')[-1].split('.')[0]
 
